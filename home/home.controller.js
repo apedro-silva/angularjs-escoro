34940d528b0fdcb99aef8b0af56a4b50e6269e57
apedro-silva <apedro.silva@gmail.com>
2020-07-01 16:52:59 +0100

    2 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -3,22 +3,47 @@ class HomeCtrl {
     'ngInject';
 
     this.name = "AngularJS";
-   
-    $scope.person = [
+    $scope.selectedPersons = [
+        {name : "Pedro",id : 2, isPreselected:false}
+    ];
+    $scope.persons = [
         {name : "Antonio",id : 1, isPreselected:false},
         {name : "Pedro",id : 2, isPreselected:false},
         {name : "Silva",id : 3, isPreselected:false}
     ];
-    $scope.persons = $scope.person.slice();
+    //$scope.persons = $scope.person.slice();
 
     $scope.filterItems = function(arr, query) {
       return arr.filter(function (el) {
         return el.id == query.id;
       })
     }
+    $scope.persons = $scope.persons.map(function (person) {
+      return {
+        isVisible : false,
+        name : person.name,
+        id : person.id,
+        isPreselected : person.isPreselected
+      }
+    });
+
     $scope.transferPerson = function() {
-      $scope.filterItems($scope.persons, $scope.selectPersons[0]).isPreselected=true;
+      $scope.persons.filter(word => $scope.selectedPersons && $scope.selectedPersons.some((element) => element==word.id)).forEach(e => e.isVisible=true);
+      // $scope.persons.forEach(p=>{
+      //   if (!$scope.selectedPersons) return;
+      //   if ($scope.selectedPersons.some((element) => element==p.id))
+      //     p.isVisible=true;
+      // });
     }
+
+    $scope.transferTS = function() {
+      $scope.persons.filter(word => $scope.selectedTS && $scope.selectedTS.some((element) => element.id==word.id)).forEach(e => e.isVisible=true);
+      // $scope.persons.forEach(p=>{
+      //   if (!$scope.selectedTS) return;
+      //   if ($scope.selectedTS.some((element) => element.id==p.id))
+      //     p.isVisible=true;
+      // });
+    }    
   }
   
 }
